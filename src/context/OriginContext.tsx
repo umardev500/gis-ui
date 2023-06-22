6ba828056443f3c6a05f2e603921fa90e52bfe9b
umar <umar.dev500@gmail.com>
2023-06-22 14:06:56 +0700

    add origin context

---
+++ src/context/OriginContext.tsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import {OriginProp} from 'src/types';
+
+export const OriginContext = React.createContext({});
+
+export interface OriginContextProp {
+  origin?: OriginProp;
+  setOrigin: React.Dispatch<React.SetStateAction<OriginProp | undefined>>;
+}
+
+interface Props {
+  children?: React.ReactNode;
+}
+export const OriginProvider: React.FC<Props> = ({children}) => {
+  const [origin, setOrigin] = useState<OriginProp>();
+  const data: OriginContextProp = {
+    origin,
+    setOrigin,
+  };
+
+  return <OriginContext.Provider value={data}>{children}</OriginContext.Provider>;
+};
